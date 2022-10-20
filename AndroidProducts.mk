3a6ed9e66c56f125ca9b01bc7cc73164977d4125
vivekgoutam2003 <108130548+vivekgoutam2003@users.noreply.github.com>
2022-10-20 11:23:42 +0530

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_Z92.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_Z92-user \
+    omni_Z92-userdebug \
+    omni_Z92-eng
